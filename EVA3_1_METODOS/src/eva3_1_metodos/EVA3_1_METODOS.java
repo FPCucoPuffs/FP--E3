d3be29e05e28376b01fed1ae26a230ef40b7652c
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-11-04 17:40:00 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_METODOS {
+    public static void main(String[] args) {
+        //USO DE LOS METODOS;
+        //INVOCACION O LLAMADA 
+        //METODO: intstrucciones, bloque de codigo
+        
+        //PUBLIC STATIC; modificadores
+        //VOID; valor de retorno
+        //() = PARAMETRO; 
+        //main(String[] args); firma, es unica y no se puede repetir
+        imprimirMensaje("Hola mundo!!");
+        
+    }
+    //DECLARACION
+    //header (encabezado)
+    //IMPRIMIR UN MENSAJE
+    //JAVA; iniciar en minusculas, verbo, siguientes palabras inician con mayuscula
+    
+    public static void imprimirMensaje (String mensaje){
+        System.out.println("TU MENSAJE: " +  mensaje);
+        
+    }
+    
+    
+}
